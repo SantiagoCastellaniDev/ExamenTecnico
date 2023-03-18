98e6be8d7c880e7234d6310823c1d0054493a761
Santiago <monitocastellani@gmail.com>
2023-03-18 18:35:13 -0300

    feat:aleert special day

--- frontend/src/app/components/fechas/fechas.component.ts
+++ frontend/src/app/components/fechas/fechas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-fechas',
@@ -8,13 +10,29 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class FechasComponent implements OnInit {
 
-  constructor(private productService:ProductsService) { }
+  constructor(private productService:ProductsService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
   generateFecha(){
-    this.productService.generatedFecha();    
+    this.productService.generatedFecha();
+    this.success();    
   }
+
+  success(){
+    Swal.fire({
+      title: 'Felicidades',
+      text: "Has decretado que hoy es un dia especial. En las compras de 10 o más productos, tendrás un descuento de $300.",
+      icon: 'success',
+      showCancelButton: false,
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'Ok'
+    }).then((result:any) => {
+      if (result.isConfirmed) {
+        this.router.navigateByUrl('/landing')
+      }
+    })
+  } 
 
 }
